29fe7faa4e5f78038660bf629e956252c42ffbd3
Matthew Wu <Matthew Wu>
2016-02-28 02:43:37 -0800

    Add open and close profile form buttons

--- tumblelog/static/scripts.js
+++ tumblelog/static/scripts.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+    $("#add-profile-btn").click(function() {
+        $("#add-profile-btn").hide();
+        $("#add-profile-form").show();
+    });
+    $("#close-profile-btn").click(function() {
+        $("#add-profile-btn").show();
+        $("#add-profile-form").hide();
+    });
+});
